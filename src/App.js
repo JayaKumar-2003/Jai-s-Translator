4c730c283ecf577d652ae541ab6bba2a2355d9ed
JayaKumar-2003 <jayakumar112234@gmail.com>
2023-03-03 16:06:07 +0530

    finshed-api-translator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import './App.css'
+function App () {
+  const [options , Setoptions] = useState([]);
+  const [to,Setto]= useState("en");
+  const [from,setfrom]= useState("en");
+  const [input,Setinput] = useState("");
+  const [output,setOutput] = useState("");
 
-function App() {
+  const translate=() =>{
+    const params = new URLSearchParams();
+  params.append('q',input);
+  params.append('source',from);
+  params.append('target',to);
+  params.append('api_key','xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx');
+
+
+  Axios.post("https://libretranslate.de/translate",params,{
+    headers :{'accept' : 'application/json',
+    'Content-Type':'application/x-www-form-urlencoded'},
+  }).then(res => {
+    console.log(res.data);
+    setOutput(res.data.translatedText);
+});
+}
+  useEffect(()=>{
+    Axios.get("https://libretranslate.de/languages").then(res => {
+      console.log(res.data);
+      Setoptions(res.data);
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <span className='brand'> Jai's Translator</span>
+      <div> <span> From {from}</span>
+      <select onChange={(e)=>setfrom(e.target.value)}>
+      {options.map(opt=><option key={opt.code} value={opt.code}> {opt.name}</option>)}
+      
+      </select>
+      </div>
+
+      <div><span>To {to} </span>
+      <select onChange={(e)=>Setto(e.target.value)}>
+      {options.map(opt=><option key={opt.code} value={opt.code}> {opt.name}</option>)}
+      </select>
+      </div>
+      <div >
+      <input type='text' onInput={(e)=>Setinput(e.target.value)}></input>
+      </div>
+      <div>
+      <input type='text' value={output}></input>
+      </div>
+      <div>
+        <button onClick={e=>translate()}>Translate</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
